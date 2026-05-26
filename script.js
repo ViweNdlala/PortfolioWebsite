ca3ac3bf81eb60683f840e3cabcdf0a964210b08
ViweNdlala <viwendlala806@gmail.com>
2026-05-26 21:45:26 +0200

    About section in progress, responsive styles needed...

--- script.js
+++ script.js
@@ -5,32 +5,9 @@ const hamburger = document.getElementsByClassName('bx-menu-wide');
 const navLinks = document.getElementsByClassName('nav-links');
 hamburger[0].addEventListener('click', hideNavLinks);
 
-
 // About details card content
 const detailsCards = document.querySelectorAll('.details-card');
-detailsCards.forEach( detailsCard =>{
-    const showcardDetails = detailsCard.querySelector('.show-card-details');
-    const hidecardDetails = detailsCard.querySelector('.hide-card-details');
-    const detailsCardContent = detailsCard.querySelector('.details-card-content');
 
-    // Initial state of arrows
-    detailsCardContent.style.display = "none";
-    hidecardDetails.style.display = "none";
-    
-    // Show details card content on clisk
-    showcardDetails.addEventListener('click', () =>{
-        detailsCardContent.style.display = "inline-block";
-        showcardDetails.style.display = "none";
-        hidecardDetails.style.display = "flex";
-    });
-
-    // Hide details card content on click
-    hidecardDetails.addEventListener('click', () =>{
-        detailsCardContent.style.display = "none";
-        showcardDetails.style.display = "flex";
-        hidecardDetails.style.display = "none";
-    });
-});
 
 
 // Hamburger function
@@ -42,11 +19,48 @@ function hideNavLinks(event){
     }
 }
 
-// About section
-// function showDetailsCard(detailsCard){
-//     if(detailsCard.classList.contains('active')){
-//         detailsCard.classList.remove('active');
-//     }else{
-//         detailsCard.classList.add('active');
-//     }
-// }
+// About details card content function
+detailsCards.forEach( detailsCard =>{
+    const showcardDetails = detailsCard.querySelector('.show-card-details');
+    const hidecardDetails = detailsCard.querySelector('.hide-card-details');
+    const detailsCardContent = detailsCard.querySelector('.details-card-content');
+
+    // // Initial state of arrows
+    // detailsCardContent.style.display = "none";
+    // hidecardDetails.style.display = "none";
+    
+    // // Show details card content on clisk
+    // showcardDetails.addEventListener('click', () =>{
+    //     detailsCardContent.style.display = "inline-block";
+    //     showcardDetails.style.display = "none";
+    //     hidecardDetails.style.display = "flex";
+    // });
+
+    // // Hide details card content on click
+    // hidecardDetails.addEventListener('click', () =>{
+    //     detailsCardContent.style.display = "none";
+    //     showcardDetails.style.display = "flex";
+    //     hidecardDetails.style.display = "none";
+    // });
+
+    // Initial state
+    hidecardDetails.style.display = "none";
+
+    // Show content
+    showcardDetails.addEventListener('click', () => {
+
+        detailsCardContent.classList.add('active');
+
+        showcardDetails.style.display = "none";
+        hidecardDetails.style.display = "flex";
+    });
+
+    // Hide content
+    hidecardDetails.addEventListener('click', () => {
+
+        detailsCardContent.classList.remove('active');
+
+        showcardDetails.style.display = "flex";
+        hidecardDetails.style.display = "none";
+    });
+});
